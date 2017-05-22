2d4c385c084997c4b91e9b1e570e9b8ebb781793
Rohit Daryanani <rohitdaryanani@yahoo.com>
2017-05-22 14:40:53 +0800

    set up redux

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+export const fetchGithubRepos = (term) => {
+  console.log(term);
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import reducers from './reducers'
+
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>, document.getElementById('root'));
