661ebc6afea8a87559d994a0a8481d5e26bb135e
Rohit Daryanani <rohitdaryanani@yahoo.com>
2017-05-22 17:15:58 +0800

    convert searchbar to stateless component

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -3,33 +3,28 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchGithubRepos } from '../actions/index'
 
-class SearchBar extends Component {
 
-  constructor(props) {
-    super(props)
-    this.searchRepos = this.searchRepos.bind(this);
-  }
-  
-  searchRepos(event) {
+const SearchBar = (props) => {
+  let textInput = '';
+ 
+  const searchRepos = (event) => {
     event.preventDefault();
-    this.props.fetchGithubRepos(this.textInput.value)
-    this.textInput.value = '';
-  }
 
-  render() {
-    return (
-      <form action="" onSubmit={this.searchRepos}>
-        <input 
-          type="text" 
-          placeholder="Search Github Repos"
-          ref={(input) => { this.textInput = input; }}/>
-        <input 
-          type="submit" 
-          value="Search" 
-          className="btn btn-primary"/>
-      </form>
-    )
+    props.fetchGithubRepos(textInput.value)
+    textInput.value = '';
   }
+  return (
+    <form onSubmit={searchRepos}>
+      <input
+        type="text" 
+        placeholder="Search Github Repos"
+        ref={(input) => {textInput = input; }}/>
+      <input 
+        type="submit" 
+        value="Search" 
+        className="btn btn-primary"/>
+    </form>
+  )
 }
 
 const mapDispatchToProps = (dispatch) => {
