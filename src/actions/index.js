8d275487b902944c8490952229191ff17e9c93f9
Rohit Daryanani <rohitdaryanani@yahoo.com>
2017-05-22 17:32:22 +0800

    add api for repo search

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
+
+const ROOT_URL = 'https://api.github.com/search/repositories';
+
 export const fetchGithubRepos = (term) => {
-  console.log(term);
+  const url = `${ROOT_URL}?q=${term}`
+  const request = axios.get(url);
   return{
-    type: 'FETCH_REPOS'
+    type: 'FETCH_REPOS',
+    payload: request
   };
 }
